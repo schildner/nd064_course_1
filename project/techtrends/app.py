cb3de7b8ee2c978501b6548d96381341f5089239
Eduard Schildner <eduard.schildner@gmail.com>
2023-12-31 16:13:49 +0100

    Add health status and metrics.

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -4,11 +4,14 @@ import logging
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+db_connections_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connections_count += 1
     app.logger.info('DB connection successfull')
     return connection
 
@@ -70,6 +73,31 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+@app.route('/healtz')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    db_connection = get_db_connection()
+    posts = db_connection.execute('SELECT COUNT(*) FROM posts').fetchone[0]
+
+    db_connection.close()
+
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"db_connection_count":db_connections_count,"post_count":posts}}),
+                          status=200,
+                          mimetype='application/json'
+    )
+
+    return response
 
 # start the application on port 3111
 if __name__ == "__main__":
